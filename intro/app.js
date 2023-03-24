60cbdd6b53e05a520671977b736d6342abaa464b
odbayar <odbayardorjsuren8@gmail.com>
2023-03-24 17:14:28 +0800

    bend

--- intro/app.js
+++ intro/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 let categories = JSON.parse(fs.readFileSync("categoryData.json", "utf-8"));
-let products = JSON.parse(fs.readFileSync("products.json", "utf-8"));
+let navbar = JSON.parse(fs.readFileSync("navbar.json", "utf-8"));
+let product = JSON.parse(fs.readFileSync("product.json", "utf-8"));
 
 const app = express();
 
@@ -14,41 +15,21 @@ const bodyParser = require("body-parser");
 app.use(cors());
 
 const port = 8000;
-const navbar = [
-  {
-    li: "Browse categories ⌄",
-    popular: [
-      {
-        pop: "1",
-      },
-      { pop: "2" },
-      { pop: "3" },
-    ],
-  },
-  {
-    li: "Home ⌄",
-  },
-  {
-    li: "Catolog ⌄",
-  },
-  {
-    li: "Blog",
-  },
-  {
-    li: "Pages ⌄",
-  },
-  {
-    li: "About us",
-  },
-];
-app.get("/products", (req, res) => {
-  res.json(products);
+
+app.use(express.static("public"));
+app.use("/images", express.static("images"));
+
+app.get("/category", (req, res) => {
+  res.json(categories);
 });
 
 app.get("/navbar", (req, res) => {
   res.json(navbar);
   // response.status(200);
   // response.json(categories);
+});
+app.get("/product", (req, res) => {
+  res.json(product);
 });
 
 app.listen(port, () => {
