2c156e8f82a726b0d0b82bca5d747bd27a3c4f45
odbayar <odbayardorjsuren8@gmail.com>
2023-04-13 18:08:21 +0800

    first commit

--- intro/app.js
+++ intro/app.js
@@ -5,6 +5,15 @@ const fs = require("fs");
 let categories = JSON.parse(fs.readFileSync("categoryData.json", "utf-8"));
 let navbar = JSON.parse(fs.readFileSync("navbar.json", "utf-8"));
 let product = JSON.parse(fs.readFileSync("product.json", "utf-8"));
+let plusCart = JSON.parse(fs.readFileSync("plusCart.json", "utf-8"));
+// let hospital = JSON.parse(fs.readFileSync("hospital.json", "utf-8"));
+let relatedProduct = JSON.parse(
+  fs.readFileSync("related-Product.json", "utf-8")
+);
+
+let updatedPlusCart = (plusCart) => {
+  fs.writeFileSync("plusCart.json", JSON.stringify(plusCart));
+};
 
 const app = express();
 
@@ -13,11 +22,16 @@ const bodyParser = require("body-parser");
 // const jsonParser = bodyParser.json();
 
 app.use(cors());
+app.use(express.json());
 
-const port = 8000;
+const port = 8080;
 
 app.use(express.static("public"));
 app.use("/images", express.static("images"));
+
+// app.get("/hospital", (req, res) => {
+//   res.json(hospital);
+// });
 
 app.get("/category", (req, res) => {
   res.json(categories);
@@ -30,6 +44,46 @@ app.get("/navbar", (req, res) => {
 });
 app.get("/product", (req, res) => {
   res.json(product);
+});
+app.get("/plusCart", (req, res) => {
+  res.json(plusCart);
+});
+app.get("/plusCart/:id", (req, res) => {
+  const { id } = req.params;
+  let filteredCart = product.find((item) => {
+    return item.id == id;
+  });
+  res.json(filteredCart);
+  console.log(filteredCart);
+});
+
+app.post("/plusCart", (req, res) => {
+  const { item } = req.body;
+  plusCart.push(item);
+  fs.writeFileSync("plusCart.json", JSON.stringify(plusCart));
+  console.log(item);
+});
+app.delete("/plusCart/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  plusCart = plusCart.filter((item) => {
+    return item.id !== Number(id);
+  });
+  updatedPlusCart(plusCart);
+  res.json(id);
+  // console.log(id);
+  res.status(200).send("Item deleted successfully");
+});
+
+app.delete("/plusCart", (req, res) => {
+  // delete all items from database
+  plusCart = plusCart.filter((item) => {
+    return item.id !== item.id;
+  });
+  updatedPlusCart(plusCart);
+});
+app.get("/relatedProduct", (req, res) => {
+  res.json(relatedProduct);
 });
 
 app.listen(port, () => {
